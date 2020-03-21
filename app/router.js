f16d07b0b5a5c9d5a0aa89403550baccf33bcde3
Benedikt Buchert <benedikt@buchert.digital>
2020-03-21 21:25:34 +0100

    add rout for question

--- app/router.js
+++ app/router.js
@@ -7,5 +7,8 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
-  this.route('fragen');
+  this.route('fragen', function() {
+    // TODO: Replace Dynamic ID once We have Data https://guides.emberjs.com/release/routing/defining-your-routes/#toc_dynamic-segments
+    this.route('frage', {path: '1'});
+  });
 });
