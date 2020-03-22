171da881bb381f0d95eb6f7700abf5b18371e0de
Marcel Müller <marcel.mueller@campus.tu-berlin.de>
2020-03-22 11:22:31 +0100

    bug fix link

--- app/routes/fragen.js
+++ app/routes/fragen.js
@@ -5,7 +5,9 @@ export default class FrageRoute extends Route {
     model(params) {
         let fragen = this.modelFor('application');
         let id = params.id;
-        return fragen[id];
+        let frage = fragen.find(obj => {
+          return obj.id == id;
+        });
+        return frage;
       }
-
 }
