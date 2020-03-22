a00177b01d9946bbad8502d8f12e02656b29539a
Benedikt Buchert <benedikt@buchert.digital>
2020-03-22 10:46:19 +0100

    properly escape body html

---
+++ app/components/frage.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+import { htmlSafe } from '@ember/template';
+
+export default Component.extend({
+    get body() {
+        return htmlSafe(this.frage.body);
+    }
+
+});
