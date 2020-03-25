67a26c972e70a4d18230ee8542bda7bd1a040194
Benedikt Buchert <benedikt@buchert.digital>
2020-03-25 16:43:06 +0100

    use tracked property in frage component to allow proper loading

--- app/components/frage.js
+++ app/components/frage.js
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
 import { htmlSafe } from '@ember/template';
+import { tracked } from "@glimmer/tracking";
 
 export default class FrageComponent extends Component {
+    @tracked frage;
 
     get body() {
+        console.log(this)
         return htmlSafe(this.frage.body);
     }
 
