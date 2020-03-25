727edfd866f659f84d9382041ff22470e2786f2f
Benedikt Buchert <benedikt@buchert.digital>
2020-03-25 19:13:25 +0100

    exclude assets folder from fingerprinting for now

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: ['assets'],
+    }
     // Add options here
   });
 
