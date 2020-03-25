99231d4cd18fdb5b43d22ff1565b9fb0865124ed
Benedikt Buchert <benedikt@buchert.digital>
2020-03-25 16:39:41 +0100

    use class syntax in components

--- app/components/frage.js
+++ app/components/frage.js
@@ -1,12 +1,13 @@
 import Component from '@ember/component';
 import { htmlSafe } from '@ember/template';
 
-export default Component.extend({
+export default class FrageComponent extends Component {
+
     get body() {
         return htmlSafe(this.frage.body);
-    },
+    }
 
     get additionalinfo() {
         return htmlSafe(this.frage.additionalinfo);
     }
-});
+}
