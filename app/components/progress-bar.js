85faade1bd375d2fd081ea198f3cbbf54a505b08
Benedikt Buchert <benedikt@buchert.digital>
2020-03-26 09:43:41 +0100

    make progressbar more accesible by showing percentage

---
+++ app/components/progress-bar.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
+
+export default class ProgressBarComponent extends Component {
+
+    get progresspercent() {
+        let percent = this.args.progress * 100;
+        return percent + "%";
+    }
+
+}
